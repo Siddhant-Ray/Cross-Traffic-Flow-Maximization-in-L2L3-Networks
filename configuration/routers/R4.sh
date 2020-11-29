66f1460a5e37fd944de7ad5fc2f316f0d3e10d20
Siddhant Ray <sidray@student.ethz.ch>
2020-11-29 20:45:32 +0000

    further ECMP

--- configuration/routers/R4.sh
+++ configuration/routers/R4.sh
@@ -24,23 +24,26 @@ EOM
 # Write your configuration below. This script will be executed on the container.
 ###############################################################################
 
+tc qdisc add dev port_R4 handle 1: root htb default 14
+tc class add dev port_R4 parent 1: classid 1:1 htb rate 12Mbit ceil 12Mbit burst 15k
+
 
 # high priority gold  TOS 128
-sudo ip netns exec router tc class add dev router-eth3 parent 1:1 classid 1:10 htb rate 1Mbit ceil 10Mbit prio 1 burst 15k cburst 15K
-sudo ip netns exec router tc filter add dev router-eth3 parent 1: protocol ip prio 1 u32 match ip protocol 1 0xff match ip dsfield 128 0xff flowid 1:10
+sudo ip netns exec router tc class add dev port_R1 parent 1:1 classid 1:10 htb rate 1Mbit ceil 10Mbit prio 1 burst 15k cburst 15K
+sudo ip netns exec router tc filter add dev port_R1 parent 1: protocol ip prio 1 u32 match ip protocol 3 0xff match ip dsfield 128 0xff flowid 1:10
 
 
 # medium  priority silver TOS 64
-sudo ip netns exec router tc class add dev router-eth3 parent 1:1 classid 1:20 htb rate 4Mbit ceil 10Mbit prio 2 burst 15k cburst 15K
-sudo ip netns exec router tc filter add dev router-eth3 parent 1: protocol ip prio 1 u32 match ip protocol 1 0xff match ip dsfield 64 0xff flowid 1:20
+sudo ip netns exec router tc class add dev port_R1 parent 1:1 classid 1:20 htb rate 4Mbit ceil 10Mbit prio 2 burst 15k cburst 15K
+sudo ip netns exec router tc filter add dev port_R1 parent 1: protocol ip prio 1 u32 match ip protocol 1 0xff match ip dsfield 64 0xff flowid 1:20
 
 
-#low  priority silver  TOS 32
-sudo ip netns exec router tc class add dev router-eth3 parent 1:1 classid 1:30 htb rate 12Mbit ceil 12Mbit prio 3 burst 15k cburst 15K
-sudo ip netns exec router tc filter add dev router-eth3 parent 1: protocol ip prio 1 u32 match ip protocol 1 0xff match ip dsfield 32  0xff flowid 1:30
+#low  priority bronze  TOS 32
+sudo ip netns exec router tc class add dev port_R1 parent 1:1 classid 1:30 htb rate 12Mbit ceil 12Mbit prio 3 burst 15k cburst 15K
+sudo ip netns exec router tc filter add dev port_R1 parent 1: protocol ip prio 1 u32 match ip protocol 1 0xff match ip dsfield 32  0xff flowid 1:30
 
 # Last qdisc
-sudo ip netns exec router tc qdisc add dev router-eth3 parent 1:10 handle 10: sfq perturb 10 limit 64 quantum 10000
-sudo ip netns exec router tc qdisc add dev router-eth3 parent 1:20 handle 20: sfq perturb 10 limit 64 quantum 10000
-sudo ip netns exec router tc qdisc add dev router-eth3 parent 1:30 handle 30: sfq perturb 10 limit 64 quantum 10000
+sudo ip netns exec router tc qdisc add dev port_R1 parent 1:10 handle 10: sfq perturb 10 limit 64 quantum 10000
+sudo ip netns exec router tc qdisc add dev port_R1 parent 1:20 handle 20: sfq perturb 10 limit 64 quantum 10000
+sudo ip netns exec router tc qdisc add dev port_R1 parent 1:30 handle 30: sfq perturb 10 limit 64 quantum 10000
 
