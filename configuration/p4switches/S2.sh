58c645ffd6f74ae550564a27a0a80ca833278598
Moritz Fuller <fullermoritz@gmail.com>
2020-12-02 10:03:35 +0000

    reflecting newest changes to project

--- configuration/p4switches/S2.sh
+++ configuration/p4switches/S2.sh
@@ -10,28 +10,90 @@ set -eu  # Exit on error (-e), treat unset variables as errors (-u).
 ###############################################################################
 
 # R1 
-tc qdisc add dev port_R1 handle 1: root htb default 1 direct_qlen 1000000
-    tc class add dev port_R1 parent 1: classid 1:1 htb rate 4Mbit ceil 4Mbit burst 15k cburst 15K
-        # not changing the perturb as chance that we have a collision is quite low and the documentation says
-        # perturbing might cause losses.
+tc qdisc add dev port_R1 handle 1: root htb default 14 direct_qlen 1000000
+tc class add dev port_R1 parent 1: classid 1:1 htb rate 4Mbit ceil 4Mbit burst 15k cburst 15k
 
-        # increasing the limit of a single queue to a very high value (although it seems that depth is responsible to
-        # to limit the packets per flow and can only be lowered)
+    ## classes 
 
-        # setting quantum to the MTU which is advised in the documentation 
+    # gold
+    tc class add dev port_R1 parent 1:1 classid 1:11 htb rate 3.9Mbit ceil 4Mbit burst 15k cburst 15K prio 1 
 
-        tc qdisc add dev port_R1 parent 1:1 handle 10: sfq limit 16256 quantum 1500
+    # silver
+    tc class add dev port_R1 parent 1:1 classid 1:12 htb rate 0.04Mbit ceil 4Mbit burst 15k cburst 15K prio 2
+
+    # bronze
+    tc class add dev port_R1 parent 1:1 classid 1:13 htb rate 0.04Mbit ceil 4Mbit burst 15k cburst 15K prio 3
+
+    # other
+    tc class add dev port_R1 parent 1:1 classid 1:14 htb rate 0.02Mbit ceil 4Mbit burst 15k cburst 15K prio 4
+
+    ## filters
+
+    # not changing the perturb as chance that we have a collision is quite low and the documentation says
+    # perturbing might cause losses.
+
+    # increasing the limit of a single queue to a very high value (although it seems that depth is responsible to
+    # to limit the packets per flow and can only be lowered)
+
+    # setting quantum to the MTU which is advised in the documentation 
+
+    # gold
+    tc filter add dev port_R1 parent 1: protocol all prio 1 u32 match ip protocol 17 0xff match ip tos 128 0xff flowid 1:11
+    
+    # silver
+    tc filter add dev port_R1 parent 1: protocol all prio 1 u32 match ip protocol 17 0xff match ip tos 64 0xff flowid 1:12
+    
+    # bronze
+    tc filter add dev port_R1 parent 1: protocol all prio 1 u32 match ip protocol 17 0xff match ip tos 32 0xff flowid 1:13
+
+    ## leaves
+
+    tc qdisc add dev port_R1 parent 1:11 handle 10: sfq limit 16256 quantum 1500
+    tc qdisc add dev port_R1 parent 1:12 handle 20: sfq limit 16256 quantum 1500
+    tc qdisc add dev port_R1 parent 1:13 handle 30: sfq limit 16256 quantum 1500
+    tc qdisc add dev port_R1 parent 1:14 handle 40: sfq limit 16256 quantum 1500
 
 
 # R2 
-tc qdisc add dev port_R2 handle 1: root htb default 1 direct_qlen 1000000
-    tc class add dev port_R2 parent 1: classid 1:1 htb rate 4Mbit ceil 4Mbit burst 15k cburst 15K
-        # not changing the perturb as chance that we have a collision is quite low and the documentation says
-        # perturbing might cause losses.
+tc qdisc add dev port_R2 handle 1: root htb default 14 direct_qlen 1000000
+tc class add dev port_R2 parent 1: classid 1:1 htb rate 4Mbit ceil 4Mbit burst 15k cburst 15k
 
-        # increasing the limit of a single queue to a very high value (although it seems that depth is responsible to
-        # to limit the packets per flow and can only be lowered)
+    ## classes 
 
-        # setting quantum to the MTU which is advised in the documentation 
+    # gold
+    tc class add dev port_R2 parent 1:1 classid 1:11 htb rate 3.9Mbit ceil 4Mbit burst 15k cburst 15K prio 1 
 
-        tc qdisc add dev port_R2 parent 1:1 handle 10: sfq limit 16256 quantum 1500
+    # silver
+    tc class add dev port_R2 parent 1:1 classid 1:12 htb rate 0.04Mbit ceil 4Mbit burst 15k cburst 15K prio 2
+
+    # bronze
+    tc class add dev port_R2 parent 1:1 classid 1:13 htb rate 0.04Mbit ceil 4Mbit burst 15k cburst 15K prio 3
+
+    # other
+    tc class add dev port_R2 parent 1:1 classid 1:14 htb rate 0.02Mbit ceil 4Mbit burst 15k cburst 15K prio 4
+
+    ## filters
+
+    # not changing the perturb as chance that we have a collision is quite low and the documentation says
+    # perturbing might cause losses.
+
+    # increasing the limit of a single queue to a very high value (although it seems that depth is responsible to
+    # to limit the packets per flow and can only be lowered)
+
+    # setting quantum to the MTU which is advised in the documentation 
+
+    # gold
+    tc filter add dev port_R2 parent 1: protocol all prio 1 u32 match ip protocol 17 0xff match ip tos 128 0xff flowid 1:11
+    
+    # silver
+    tc filter add dev port_R2 parent 1: protocol all prio 1 u32 match ip protocol 17 0xff match ip tos 64 0xff flowid 1:12
+    
+    # bronze
+    tc filter add dev port_R2 parent 1: protocol all prio 1 u32 match ip protocol 17 0xff match ip tos 32 0xff flowid 1:13
+
+    ## leaves
+
+    tc qdisc add dev port_R2 parent 1:11 handle 10: sfq limit 16256 quantum 1500
+    tc qdisc add dev port_R2 parent 1:12 handle 20: sfq limit 16256 quantum 1500
+    tc qdisc add dev port_R2 parent 1:13 handle 30: sfq limit 16256 quantum 1500
+    tc qdisc add dev port_R2 parent 1:14 handle 40: sfq limit 16256 quantum 1500
